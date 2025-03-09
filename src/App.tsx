65ea209b33218bd44b74e6032e8b4424fd3c8468
samjmccormick <samjmccormick@gmail.com>
2025-03-09 12:49:28 -0700

    color picker linked to state

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import "./App.css";
 function App() {
   const [avatarColor, setAvatarColor] = useState("#000000");
 
+  function handleColorChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setAvatarColor(event.target.value);
+  }
+
   return (
     <div className="container d-flex align-items-center h-100">
       <div className="d-flex flex-row justify-content-evenly align-content-center align-items-center fixed-top header">
@@ -12,21 +16,30 @@ function App() {
       </div>
       <div
         id="avatar-container"
-        className="d-flex justify-content-center  flex-column align-items-center w-75 mx-auto p-3"
+        className="d-flex justify-content-center  flex-column align-items-center w-75 mx-auto p-4"
         style={{ backgroundColor: avatarColor }}
       >
         <img
           src="https://robohash.org/asd"
           alt="Avatar"
           id="avatar"
-          className="border border-secondary w-75"
+          className="border border-secondary w-75 mb-3"
         />
-        <form className="d-flex flex-column align-items-center">
-          <input className="row" type="color" id="avatar-color"></input>
-          <button type="button" className="btn btn-primary row">
-            Reset
-          </button>
-        </form>
+
+        <input
+          className="row mb-2"
+          type="color"
+          id="avatar-color"
+          value={avatarColor}
+          onChange={handleColorChange}
+        ></input>
+        <button
+          type="button"
+          className="btn row "
+          onClick={() => setAvatarColor("#000000")}
+        >
+          Reset
+        </button>
       </div>
       <div className="d-flex flex-row justify-content-evenly align-items-center fixed-bottom footer">
         <div className="col">2025 Playtoon Inc</div>
